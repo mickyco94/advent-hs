983a3fe1cb46ac0e4f0810f3aebef2bec0a25fd2
mickyco94 <mickyclerkinoliver@gmail.com>
2025-05-25 17:30:10 +0100

    day 2: part 1

--- src/day02/Main.hs
+++ src/day02/Main.hs
@@ -1,4 +1,38 @@
 module Main where
 
+import System.Environment (getArgs)
+
+withPrevious :: [a] -> [(a, a)]
+withPrevious l = zip l (drop 1 l)
+
+decreasing :: (Ord a) => [a] -> Bool
+decreasing input = all (uncurry (>)) (withPrevious input)
+
+increasing :: (Ord a) => [a] -> Bool
+increasing input = all (uncurry (<)) (withPrevious input)
+
+safe :: (Num a, Ord a) => [a] -> Bool
+safe input = all (\(x, y) -> 0 < abs (x - y) && abs (x - y) < 4) (withPrevious input)
+
+readInput :: String -> IO [[Int]]
+readInput path = do
+  contents <- readFile path
+  return (map parseLine (lines contents))
+
+parseLine :: String -> [Int]
+parseLine = map read . words
+
+valid :: (Num a, Ord a) => [a] -> Bool
+valid l = safe l && (decreasing l || increasing l)
+
+solve :: [[Int]] -> Int
+solve = length . filter valid
+
 main :: IO ()
-main = putStrLn "Day 02 not implemented yet"
+main = do
+  args <- getArgs
+  case args of
+    [path] -> do
+      reports <- readInput path
+      print $ solve reports
+    _ -> putStrLn "Please provide input file"
