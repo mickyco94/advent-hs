adb1aa09581ec803d40e6b03cde3b40ae02d3584
mickyco94 <mickyclerkinoliver@gmail.com>
2025-05-25 16:27:42 +0100

    day 1: part 2

--- src/day01/Main.hs
+++ src/day01/Main.hs
@@ -1,5 +1,7 @@
+import qualified Data.HashMap.Internal.Strict as Map
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable (Hashable)
 import Data.List (sort)
-import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
@@ -24,17 +26,32 @@ dist = sum . map diff
 diff :: (Int, Int) -> Int
 diff (x, y) = abs (x - y)
 
-solve :: String -> Maybe Int
-solve contents = do
-  pairs <- traverse parse (lines contents)
-  let sortedPairs = sortPairs pairs
-  return (dist sortedPairs)
+totalDist :: [(Int, Int)] -> Int
+totalDist pairs = dist (sortPairs pairs)
+
+countElems :: (Eq a, Hashable a) => [a] -> HM.HashMap a Int
+countElems = foldr (\x acc -> HM.insertWith (+) x 1 acc) HM.empty
+
+similarity :: [(Int, Int)] -> Int
+similarity pairs =
+  let a = map fst pairs
+      bMap = countElems (map snd pairs)
+      similarElements = map (\key -> key * Map.findWithDefault 0 key bMap) a
+   in sum similarElements
+
+solve :: String -> IO ()
+solve path = do
+  input <- readFile path
+  let maybePairs = traverse parse (lines input)
+  case maybePairs of
+    Just pairs -> do
+      print $ totalDist pairs
+      print $ similarity pairs
+    Nothing -> putStrLn "Invalid input file"
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [path] -> do
-      contents <- readFile path
-      print $ fromMaybe 0 (solve contents)
+    [path] -> solve path
     _ -> putStrLn "Please provide an input file"
