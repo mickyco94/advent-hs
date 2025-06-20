e9308c71d6843ca8df85c2424313964bed5665cd
mickyco94 <mickyclerkinoliver@gmail.com>
2025-06-20 22:20:22 +0100

    day 6: part 2

--- src/day06/Main.hs
+++ src/day06/Main.hs
@@ -1,19 +1,26 @@
 module Main where
 
-import Data.List (find)
+import Data.Array
+import Data.Foldable (find)
 import qualified Data.Set as Set
 import System.Environment (getArgs)
 
-data Direction = U | D | L | R deriving (Show, Eq)
+data Direction = U | D | L | R deriving (Show, Eq, Ord)
 
 data Tile = Dot | Hash | Guard Direction deriving (Show, Eq)
 
-type Map = [[Tile]]
+type Map = Array (Int, Int) Tile
 
 type Position = (Int, Int)
 
 parse :: String -> Map
-parse s = map (map charToTile) (lines s)
+parse s = array b entries
+  where
+    rows = lines s
+    numRows = length rows
+    numCols = length (head rows)
+    b = ((0, 0), (numRows - 1, numCols - 1))
+    entries = [((i, j), charToTile cell) | (i, row) <- zip [0 ..] rows, (j, cell) <- zip [0 ..] row]
 
 charToTile :: Char -> Tile
 charToTile '.' = Dot
@@ -29,13 +36,10 @@ isGuard (Guard _) = True
 isGuard _ = False
 
 obstacles :: Map -> [Position]
-obstacles m = [(x, y) | (x, row) <- zip [0 ..] m, (y, cell) <- zip [0 ..] row, cell == Hash]
+obstacles m = [(x, y) | ((x, y), cell) <- assocs m, cell == Hash]
 
 findInMap :: (Tile -> Bool) -> Map -> Maybe (Position, Tile)
-findInMap p m =
-  find (p . snd) indexed
-  where
-    indexed = [((x, y), val) | (x, row) <- zip [0 ..] m, (y, val) <- zip [0 ..] row]
+findInMap p m = find (p . snd) (assocs m)
 
 solve :: String -> Maybe Int
 solve s = do
@@ -62,14 +66,26 @@ walk m = go Set.empty
   where
     go :: Set.Set Position -> Position -> Direction -> Set.Set Position
     go s pos dir
-      | outOfBounds pos = s
+      | not (inRange (bounds m) pos) = s
       | move pos dir `elem` obstacles m = go s pos (shiftDir dir)
       | otherwise = go (Set.insert pos s) (move pos dir) dir
-      where
-        outOfBounds (x, y) = x < 0 || x >= length m || y >= length (head m) || y < 0
 
--- I need distinct positions, so I should collect a set of tuples rather than
--- counting
+isLoop :: Map -> Position -> Direction -> Bool
+isLoop m start startD = go start startD Set.empty
+  where
+    go pos dir visited
+      | Set.member (pos, dir) visited = True
+      | not (inRange (bounds m) pos) = False
+      | move pos dir `elem` obstacles m = go pos (shiftDir dir) (Set.insert (pos, dir) visited)
+      | otherwise = go (move pos dir) dir (Set.insert (pos, dir) visited)
+
+solvePartTwo :: String -> Maybe Int
+solvePartTwo s = do
+  let m = parse s
+  (start, Guard dir) <- findInMap isGuard m
+  let candidates = Set.delete start (walk m start dir)
+  let options = [pos | pos <- Set.toList candidates, isLoop (m // [(pos, Hash)]) start dir]
+  return (length options)
 
 main :: IO ()
 main = do
@@ -78,4 +94,5 @@ main = do
     [path] -> do
       raw <- readFile path
       print $ solve raw
+      print $ solvePartTwo raw
     _ -> putStrLn "no input provided"
