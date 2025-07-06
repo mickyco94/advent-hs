f959b2d0c708edd07eeb506c3e21833fd83816f2
mickyco94 <mickyclerkinoliver@gmail.com>
2025-07-06 18:23:59 +0100

    feat: day 9 part 2

--- src/day09/Main.hs
+++ src/day09/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import Data.Char (isNumber)
-import Data.List (find)
+import Data.Foldable (find)
+import Data.List (nub, sortBy)
 import Data.Maybe (fromMaybe)
+import Data.Ord (Down (Down), comparing)
 import System.Environment (getArgs)
 
 data Block = Free | ID ID deriving (Show, Eq)
@@ -11,38 +13,36 @@ type Size = Int
 
 type ID = Int
 
-data DiskMap
+type File = (ID, Size)
+
+type DiskMap = [Chunk]
+
+data Chunk
   = -- | Free indicates a Free block
-    FreeMap Size
+    ChunkFree Size
   | -- | Data is a contiguous Block of the given size and ID
-    DataMap ID Size
+    ChunkFile ID Size
   deriving (Show, Eq)
 
-readMap :: String -> [DiskMap]
+readMap :: String -> [Chunk]
 readMap s = go s True 0
   where
-    go :: String -> Bool -> Int -> [DiskMap]
+    go :: String -> Bool -> Int -> [Chunk]
     go "" _ _ = []
     go (c : cs) isData currentId
       | not (isNumber c) = go cs isData currentId
-      | isData = DataMap currentId (read [c]) : go cs (not isData) (currentId + 1)
-      | otherwise = FreeMap (read [c]) : go cs (not isData) currentId
+      | isData = ChunkFile currentId (read [c]) : go cs (not isData) (currentId + 1)
+      | otherwise = ChunkFree (read [c]) : go cs (not isData) currentId
 
-toBlock :: DiskMap -> [Block]
-toBlock (FreeMap 0) = []
-toBlock (DataMap _ 0) = []
-toBlock (FreeMap size) = Free : toBlock (FreeMap (size - 1))
-toBlock (DataMap idd size) = ID idd : toBlock (DataMap idd (size - 1))
+toBlock :: Chunk -> [Block]
+toBlock (ChunkFree 0) = []
+toBlock (ChunkFile _ 0) = []
+toBlock (ChunkFree size) = Free : toBlock (ChunkFree (size - 1))
+toBlock (ChunkFile idd size) = ID idd : toBlock (ChunkFile idd (size - 1))
 
 shiftOne :: [Block] -> [Block]
 shiftOne [] = []
-shiftOne (b : bs) = case b of
-  Free -> last bs : init bs ++ [Free]
-  ID _ -> b : shiftOne bs
-
-shiftOneSpan :: [Block] -> [Block]
-shiftOneSpan [] = []
-shiftOneSpan bs = start ++ swapEnd middle ++ end
+shiftOne bs = start ++ swapEnd middle ++ end
   where
     (start, rest) = span (/= Free) bs
     rrest = reverse rest
@@ -51,13 +51,10 @@ shiftOneSpan bs = start ++ swapEnd middle ++ end
 
 completed :: [Block] -> Bool
 completed [] = True
-completed (x : xs) = case x of
-  Free -> not $ isId `any` xs
-  _ -> completed xs
-
-isId :: Block -> Bool
-isId (ID _) = True
-isId _ = False
+completed [_] = True
+completed (x : y : xs) = case (x, y) of
+  (Free, ID _) -> False
+  (_, _) -> completed (y : xs)
 
 swapEnd :: [a] -> [a]
 swapEnd [] = []
@@ -66,12 +63,15 @@ swapEnd (x : xs) = last xs : init xs ++ [x]
 showBlocks :: [Block] -> String
 showBlocks = concatMap blockToStr
 
+showMap :: [Chunk] -> String
+showMap = showBlocks . concatMap toBlock
+
 blockToStr :: Block -> String
 blockToStr Free = "."
 blockToStr (ID i) = show i
 
 order :: [Block] -> [Block]
-order bs = fromMaybe bs (find completed (iterate shiftOneSpan bs))
+order bs = fromMaybe bs (find completed (iterate shiftOne bs))
 
 checksum :: [Block] -> Int
 checksum blocks = go (zip [0 ..] blocks) 0
@@ -88,11 +88,46 @@ solve s = checksum (order blocks)
     diskMap = readMap s
     blocks = concatMap toBlock diskMap
 
+-- | empty replaces the given ID with Free space
+empty :: [Chunk] -> ID -> [Chunk]
+empty chunks iid = go chunks
+  where
+    go [] = []
+    go (ChunkFile fileID size : xs)
+      | fileID == iid = ChunkFree size : go xs
+      | otherwise = ChunkFile fileID size : go xs
+    go (x : xs) = x : go xs
+
+tryMoveChunk :: ID -> Size -> [Chunk] -> [Chunk]
+tryMoveChunk _ _ [] = []
+tryMoveChunk fileID size (x : xs)
+  | ChunkFile fID _ <- x, fID == fileID = x : xs
+  | ChunkFree emptySpace <- x, size == emptySpace = ChunkFile fileID size : emptiedTail
+  | ChunkFree emptySpace <- x, size <= emptySpace = [ChunkFile fileID size, ChunkFree (emptySpace - size)] ++ emptiedTail
+  | otherwise = x : tryMoveChunk fileID size xs
+  where
+    emptiedTail = empty xs fileID
+
+decreasingChunks :: [Chunk] -> [(ID, Size)]
+decreasingChunks cs = sortBy (comparing Data.Ord.Down) $ nub [(fileID, size) | ChunkFile fileID size <- cs]
+
+moveAll :: [Chunk] -> [Chunk]
+moveAll cs = foldl (\acc (x, y) -> tryMoveChunk x y acc) cs (decreasingChunks cs)
+
+readBlocks :: String -> [Block]
+readBlocks s = concatMap toBlock (readMap s)
+
+partTwo :: String -> Int
+partTwo s = checksum $ concatMap toBlock updatedMap
+  where
+    updatedMap = moveAll (readMap s)
+
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [path] -> do
       raw <- readFile path
-      print $ solve raw
+      -- print $ solve raw
+      print $ partTwo raw
     _ -> print "no input provided"
