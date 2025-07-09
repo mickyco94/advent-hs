5b4f255c8bc9aa22108c2773b6bbab322d0c4b5a
mickyco94 <mickyclerkinoliver@gmail.com>
2025-07-09 23:37:13 +0100

    day 10: part 2

--- src/day10/Main.hs
+++ src/day10/Main.hs
@@ -1,10 +1,5 @@
 module Main where
 
--- Input has 9 trailheads
--- A trail is a path from 0 -> 9 that can be walked in any orthogonal dir
--- The score of a trailhead is given by the number of unique trails that start from that pos
--- The solution is the sum of the trailhead scores
---
 -- 89010123
 -- 78121874
 -- 87430965
@@ -13,15 +8,12 @@ module Main where
 -- 32019012
 -- 01329801
 -- 10456732
---
---
-import Data.Array
-import Data.List (nub)
-import qualified Data.Set as Set
-import Debug.Trace (trace, traceShow)
-import Foreign (new)
 
-type Map = Array Pos Int
+import Data.Array
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+type Grid = Array Pos Int
 
 type Pos = (Int, Int)
 
@@ -31,7 +23,7 @@ readChar :: Char -> Int
 readChar '.' = -1
 readChar x = read [x]
 
-parse :: String -> Map
+parse :: String -> Grid
 parse s = array b entries
   where
     rows = lines s
@@ -40,46 +32,50 @@ parse s = array b entries
     b = ((0, 0), (numRows - 1, numCols - 1))
     entries = [((i, j), readChar c) | (i, row) <- zip [0 ..] rows, (j, c) <- zip [0 ..] row]
 
--- I don't want to find the first path, I want to find all. So I think I need a queue
-
 neighbours :: Pos -> [Pos]
-neighbours (y, x) =
-  [ (yy, xx)
-    | (yy, xx) <- [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]
-  ]
+neighbours (y, x) = [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]
 
--- I probably need a visited set, or I just dedup
+walk :: Grid -> Pos -> Map Pos Int
+walk m start = foldl (step m) (Map.singleton start 1) [1 .. 9]
 
-walk :: Map -> Pos -> [Pos]
-walk m start = nub $ go [start]
-  where
-    go :: [Pos] -> [Pos]
-    go [] = []
-    go (x : xs) = x : go (xs ++ incNP)
-      where
-        curr = m ! x -- Current
-        ibn = filter (inRange (bounds m)) (neighbours x) -- in bounds possibilites
-        ibnV = map (\t -> (t, m ! t)) ibn -- in bounds possibilites values
-        incN = filter (\t -> snd t == curr + 1) ibnV -- in bounds increasing values
-        incNP = map fst incN -- The positions of the above
+step :: Grid -> Map Pos Int -> Int -> Map Pos Int
+step g m target =
+  Map.fromListWith
+    (+)
+    [ (neighbour, n)
+      | (y, n) <- Map.assocs m,
+        neighbour <- neighbours y,
+        next <- idx g neighbour,
+        next == target
+    ]
 
-heads :: Map -> [Pos]
+idx :: Grid -> Pos -> [Int]
+idx m p = [m ! p | inRange (bounds m) p]
+
+heads :: Grid -> [Pos]
 heads m = [p | (p, x) <- assocs m, x == 0]
 
-fullWalks :: Map -> Pos -> Int
-fullWalks m start = length dedupedwalk
-  where
-    w = walk m start
-    only_nines = filter (\t -> (m ! t) == 9) w
-    dedupedwalk = nub only_nines
+paths :: Grid -> [Map Pos Int]
+paths g = [walk g start | start <- heads g]
+
+rating :: Map Pos Int -> Int
+rating acc = sum $ Map.elems acc
+
+score :: Map Pos Int -> Int
+score acc = length $ Map.elems acc
+
+totalScore :: Grid -> Int
+totalScore g = sum (map score (paths g))
+
+totalRating :: Grid -> Int
+totalRating g = sum (map rating (paths g))
 
 solve :: String -> Int
-solve s = sum walks
+solve s = sum (map sum (paths m))
   where
     m = parse s
-    walks = map (fullWalks m) (heads m)
 
 main :: IO ()
 main = do
   raw <- readFile "test-input.txt"
-  print raw
+  print $ solve raw
